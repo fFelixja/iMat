ae11bc0df06b0134f4c546b2b13748f91f4b7c36
John <exodusx32@gmail.com>
2016-02-17 14:53:21 +0100

    Changed Text to Label in ProductView.fxml. Added fxid in ProductViewController

--- src/ProductViewController.java
+++ src/ProductViewController.java
@@ -1,5 +1,18 @@
-/**
- * Created by john on 2016-02-17.
- */
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+
 public class ProductViewController {
+    @FXML private Spinner amountSpinner;
+
+    @FXML private Label nameLabel;
+
+    @FXML private Label priceUnitLabel;
+
+    @FXML private Label amountUnitLabel;
+
+    @FXML private Button addToCartButton;
+
 }
