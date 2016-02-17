39f080f59d123e8533d30b2c4538ee4ee4dc63dd
John <exodusx32@gmail.com>
2016-02-17 15:17:23 +0100

    fxid eller nåt

---
+++ src/TopMenuController.java
@@ -0,0 +1,17 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class TopMenuController {
+    @FXML private TextField searchTextField;
+
+    @FXML private Button backButton;
+
+    @FXML private Button homeButton;
+
+    @FXML private Button profileButton;
+
+    @FXML private Button purchaseHistoryButton;
+
+    @FXML private Button shoppinCartButton;
+}
