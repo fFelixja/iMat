0f26492195617c34790dfb6c8b7c91e9853465cb
John <exodusx32@gmail.com>
2016-02-17 15:55:07 +0100

    DET FUNKAR

--- src/CategoryController.java
+++ src/CategoryController.java
@@ -1,8 +1,9 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
-public class CategoryController {
+public class CategoryController extends AnchorPane {
     @FXML private ImageView categoryImage;
 
     @FXML private Label categoryLabel;
--- src/ProductViewController.java
+++ src/ProductViewController.java
@@ -3,8 +3,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
+import javafx.scene.layout.AnchorPane;
 
-public class ProductViewController {
+public class ProductViewController extends AnchorPane {
     @FXML private Spinner amountSpinner;
 
     @FXML private Label nameLabel;
