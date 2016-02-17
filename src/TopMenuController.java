6cc77db06f1e66b43bcfec78a4fd6d18726aab7c
John <exodusx32@gmail.com>
2016-02-17 16:38:45 +0100

    Fuck git

--- src/TopMenuController.java
+++ src/TopMenuController.java
@@ -1,11 +1,17 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class TopMenuController extends AnchorPane {
+
+public class TopMenuController extends AnchorPane implements Initializable{
     @FXML private TextField searchTextField;
 
     @FXML private Button backButton;
@@ -16,7 +22,15 @@ public class TopMenuController extends AnchorPane {
 
     @FXML private Button purchaseHistoryButton;
 
-    @FXML private Button shoppinCartButton;
+    @FXML private Button shoppingCartButton;
+
+    @FXML private ImageView backButtonImage;
+
+    @Override
+    public void initialize(URL url, ResourceBundle bundle){
+        System.out.println("a");
+//        backButtonImage.setImage(new Image("img/backbutton.png"));
+    }
 
     @FXML
     protected void homeButtonActionPerformed(ActionEvent event) {
