51f79854fed14507678790a1e28b3445c6057357
Timbertea <sabrina.sabbe@hotmail.com>
2016-02-24 11:16:43 +0100

    Added fxmlloader to PastPurchasePanel

--- src/PastPurchasePanelController.java
+++ src/PastPurchasePanelController.java
@@ -1,9 +1,13 @@
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
+
 public class PastPurchasePanelController extends AnchorPane {
+
     @FXML private Label datePurchasedtLabel;
 
     @FXML private Label nbrOfProductsLabel;
@@ -11,5 +15,18 @@ public class PastPurchasePanelController extends AnchorPane {
     @FXML private Label priceLabel;
 
     @FXML private Button viewDetailsButton;
+
+    public PastPurchasePanelController () {
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("FXML/PastPurchasePanel.fxml"));
+            loader.setController(this);
+            loader.setRoot(this);
+            loader.load();
+        } catch (IOException e) {
+            System.out.println("fel");
+        }
+
+    }
 
 }
