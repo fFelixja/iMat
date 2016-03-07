703fe8679479a79ee5f98d957e36565bdd02c258
anderssonjohn <exodusx32@gmail.com>
2016-03-07 16:38:59 +0100

    Remove unnecessary local variables

--- src/DataHandler.java
+++ src/DataHandler.java
@@ -12,117 +12,109 @@ public class DataHandler {
     }
 
     public static String getCategory(int productId) {
-        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
-        return dataHandler.getProduct(productId).getCategory().toString();
+        return IMatDataHandler.getInstance().getProduct(productId).getCategory().toString();
     }
 
     public static Image getCategoryImage(ProductCategory category) {
-        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
-        return dataHandler.getFXImage(dataHandler.getProducts(category).get(0));
+        return IMatDataHandler.getInstance().getFXImage(IMatDataHandler.getInstance().getProducts(category).get(0));
     }
 
     public static Image getProductImage(Product product) {
-        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
-        return dataHandler.getFXImage(product);
+        return IMatDataHandler.getInstance().getFXImage(product);
     }
 
     public static List<Product> searchProducts(String search) {
-        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
-        return dataHandler.findProducts(search);
+        return IMatDataHandler.getInstance().findProducts(search);
     }
 
+
+
+
+
+
+
+
     public static void saveCustomer(String firstname, String lastname, String adress, String zipcode, String postAdress, String telephone){
-        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
         //Save customer information
-        dataHandler.getCustomer().setFirstName(firstname);
-        dataHandler.getCustomer().setLastName(lastname);
-        dataHandler.getCustomer().setAddress(adress);
-        dataHandler.getCustomer().setPostCode(zipcode);
-        dataHandler.getCustomer().setPostAddress(postAdress);
-        dataHandler.getCustomer().setPhoneNumber(telephone);
+        IMatDataHandler.getInstance().getCustomer().setFirstName(firstname);
+        IMatDataHandler.getInstance().getCustomer().setLastName(lastname);
+        IMatDataHandler.getInstance().getCustomer().setAddress(adress);
+        IMatDataHandler.getInstance().getCustomer().setPostCode(zipcode);
+        IMatDataHandler.getInstance().getCustomer().setPostAddress(postAdress);
+        IMatDataHandler.getInstance().getCustomer().setPhoneNumber(telephone);
     }
 
     public static void saveCard(String cardType, String cardNumber, String cardHolder, int expireMonth, int expireYear, int ccv ){
-        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
         //Save all card details.
-        dataHandler.getCreditCard().setCardType(cardType);
-        dataHandler.getCreditCard().setCardNumber(cardNumber);
-        dataHandler.getCreditCard().setHoldersName(cardHolder);
+        IMatDataHandler.getInstance().getCreditCard().setCardType(cardType);
+        IMatDataHandler.getInstance().getCreditCard().setCardNumber(cardNumber);
+        IMatDataHandler.getInstance().getCreditCard().setHoldersName(cardHolder);
         //if values is zero => string == null and will not be included in save
         if(expireMonth != 0) {
-            dataHandler.getCreditCard().setValidMonth(expireMonth);
+            IMatDataHandler.getInstance().getCreditCard().setValidMonth(expireMonth);
         }
         if (expireYear != 0) {
-            dataHandler.getCreditCard().setValidYear(expireYear);
+            IMatDataHandler.getInstance().getCreditCard().setValidYear(expireYear);
         }
         if (ccv != 0 ) {
-            dataHandler.getCreditCard().setVerificationCode(ccv);
+            IMatDataHandler.getInstance().getCreditCard().setVerificationCode(ccv);
         }
 
     }
 
     public static Customer getCustomer(){
-        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
-        return dataHandler.getCustomer();
+        return IMatDataHandler.getInstance().getCustomer();
     }
     public static CreditCard getCreditCard(){
-        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
-        return dataHandler.getCreditCard();
+        return IMatDataHandler.getInstance().getCreditCard();
     }
 
     public static void shutdown() {
-        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
-        dataHandler.shutDown();
+        IMatDataHandler.getInstance().shutDown();
 
     }
 
     public static ShoppingCart getShoppingCart() {
-        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
-        return dataHandler.getShoppingCart();
+        return IMatDataHandler.getInstance().getShoppingCart();
     }
 
     public static void addToCart(Product product, double amount) {
-        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
 
         //Checks if the shopping cart already contains the product as to not create duplicates
-        if (dataHandler.getShoppingCart().getItems().size() < 1) {
-            dataHandler.getShoppingCart().addProduct(product, amount);
+        if (IMatDataHandler.getInstance().getShoppingCart().getItems().size() < 1) {
+            IMatDataHandler.getInstance().getShoppingCart().addProduct(product, amount);
         } else {
             boolean productExists = false;
-            for (ShoppingItem shoppingItem : dataHandler.getShoppingCart().getItems()) {
+            for (ShoppingItem shoppingItem : IMatDataHandler.getInstance().getShoppingCart().getItems()) {
                 if (shoppingItem.getProduct().equals(product)) {
-                    dataHandler.getShoppingCart().getItems().get(
-                            dataHandler.getShoppingCart().getItems().indexOf(shoppingItem)).setAmount(
+                    IMatDataHandler.getInstance().getShoppingCart().getItems().get(
+                            IMatDataHandler.getInstance().getShoppingCart().getItems().indexOf(shoppingItem)).setAmount(
                             shoppingItem.getAmount() + amount);
                     productExists = true;
                     break;
                 }
             }
             if (!productExists) {
-                dataHandler.getShoppingCart().addProduct(product, amount);
+                IMatDataHandler.getInstance().getShoppingCart().addProduct(product, amount);
             }
         }
     }
 
     public static Product getProduct(int idNumber) {
-        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
-        return dataHandler.getProduct(idNumber);
+        return IMatDataHandler.getInstance().getProduct(idNumber);
     }
 
     public static List<Product> getProducts(ProductCategory category) {
-        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
-        return dataHandler.getProducts(category);
+        return IMatDataHandler.getInstance().getProducts(category);
 
     }
 
     public static List<Order> getPastOrders(){
-        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
-        return dataHandler.getOrders();
+        return IMatDataHandler.getInstance().getOrders();
     }
 
     public static void removeShoppingItem(ShoppingItem shoppingItem) {
-        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
-        dataHandler.getShoppingCart().removeItem(shoppingItem);
+        IMatDataHandler.getInstance().getShoppingCart().removeItem(shoppingItem);
 
     }
 
@@ -136,8 +128,7 @@ public class DataHandler {
     }
 
     public static void placeOrder() {
-        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
-        dataHandler.placeOrder(true);
-        System.out.println(dataHandler.getOrders().size());
+        IMatDataHandler.getInstance().placeOrder(true);
+        System.out.println(IMatDataHandler.getInstance().getOrders().size());
     }
 }
