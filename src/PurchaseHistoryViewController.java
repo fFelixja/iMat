2937f84150c1e4c4e3eb2586403ca59179d33b2c
John <exodusx32@gmail.com>
2016-03-09 13:40:00 +0100

    Orders listed by date

--- src/PurchaseHistoryViewController.java
+++ src/PurchaseHistoryViewController.java
@@ -1,12 +1,11 @@
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import se.chalmers.ait.dat215.project.Order;
 
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.List;
 
 public class PurchaseHistoryViewController extends AnchorPane {
@@ -26,11 +25,19 @@ public class PurchaseHistoryViewController extends AnchorPane {
 
         List<Order> pastOrders = DataHandler.getPastOrders();
 
+        pastOrders.sort(new OrderComparator());
         for (Order order : pastOrders){
+            System.out.println(order.getOrderNumber());
             purchaseBox.getChildren().add(new PastPurchasePanelController(order, controller));
         }
+    }
+}
 
+class OrderComparator implements Comparator<Order> {
 
+    @Override
+    public int compare(Order a, Order b) {
+        return b.getOrderNumber() - a.getOrderNumber();
     }
 
 }
